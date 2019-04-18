03849a54525aa0e737857482d701486eab850556
KadoTomohiro <kado.tomohiro@jp.fujitsu.com>
2019-04-18 17:23:22 +0900

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mdx-np-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ngPractice';
+}
